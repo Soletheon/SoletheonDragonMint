4e3c70756d57f2bcd6e1cd394ba9da5041661bf6
PXLFuSSeL <sbegaa@gmail.com>
2024-01-10 22:00:49 +0100

    smaller changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
+
+import pngOrganic from "./assets/ORGANIC.png"
+import pngMechanical from "./assets/MECHANICAL.png";
+import pngCyborg from "./assets/CYBORG.png";
+import pngLegendary from "./assets/LEGENDARY.png";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img className='eggsImg' src={pngOrganic}/><img className='eggsImg' src={pngMechanical} /><img className='eggsImg' src={pngCyborg} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
